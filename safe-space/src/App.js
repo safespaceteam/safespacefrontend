7c4216aae0b6b8ccdd406a2fb12d5c317f593e49
Brian G <brian.gregorius@gmail.com>
2019-03-11 12:53:35 -0400

    added create react app and installed material ui

--- safe-space/src/App.js
+++ safe-space/src/App.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+import Button from "@material-ui/core/Button";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
+            <Button variant="contained" color="primary">
+              testing
+            </Button>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
