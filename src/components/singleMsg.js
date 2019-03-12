a6d7dec16752d37ad3bb82f28a05384fc20a60ff
Brian G <brian.gregorius@gmail.com>
2019-03-12 09:18:50 -0400

    turned singleMsg into holding state

--- src/components/singleMsg.js
+++ src/components/singleMsg.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 
-const SingleMsg = props => {
-  return (
-    <div>
-      {props.msg.message}
-      <i className="material-icons">edit</i>
-      <i className="material-icons">close</i>
-      <i className="material-icons">delete_outline</i>
-    </div>
-  );
-};
+class SingleMsg extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      edit: false,
+      editMsg: ""
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.msg.message}
+        <EditMessage msg={this.props.msg} />
+        <i className="material-icons">edit</i>
+        <i className="material-icons">close</i>
+        <i className="material-icons">delete_outline</i>
+      </div>
+    );
+  }
+}
 
 export default SingleMsg;
