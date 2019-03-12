8bacf0b204f0d49e8dad81eec4d8c4ad8c48c466
Brian G <brian.gregorius@gmail.com>
2019-03-12 10:06:18 -0400

    added edit form

--- src/components/editMessage.js
+++ src/components/editMessage.js
@@ -1,7 +1,21 @@
 import React from "react";
+import TextField from "@material-ui/core/TextField";
 
 const EditMessage = props => {
-  return <div />;
+  return (
+    <form>
+      <TextField
+        id="standard-multiline-static"
+        name="editMsg"
+        type="text"
+        multiline
+        rows="4"
+        value={props.msg.message}
+        // className={classes.textField}
+        margin="normal"
+      />
+    </form>
+  );
 };
 
 export default EditMessage;
--- src/components/messages.js
+++ src/components/messages.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import EditMessage from "./editMessage";
+import axios from "axios";
+
+const url = process.env.REACT_APP_DB_URL;
 
 class Messages extends Component {
   constructor(props) {
@@ -10,14 +13,40 @@ class Messages extends Component {
     };
   }
 
+  openEditForm = () => {
+    this.setState({ edit: !this.state.edit });
+  };
+
+  deleteMsg = event => {
+    event.preventDefault();
+    axios
+      .delete(`${url}/messages/${this.props.msg.id}`)
+      .then(res => {})
+      .catch(err => {
+        throw new Error();
+      });
+  };
+
+  editMsgHandler = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
   render() {
     return (
       <div>
+        {this.state.edit ? (
+          <EditMessage msg={this.props.msg} editMsg={this.editMsgHandler} />
+        ) : null}
         {this.props.msg.message}
-        <EditMessage msg={this.props.msg} />
-        <i className="material-icons">edit</i>
-        <i className="material-icons">close</i>
-        <i className="material-icons">delete_outline</i>
+        <i className="material-icons" onClick={this.openEditForm}>
+          edit
+        </i>
+        <i className="material-icons" onClick={this.deleteMsg}>
+          close
+        </i>
+        <i className="material-icons" onClick={this.deleteMsg}>
+          delete_outline
+        </i>
       </div>
     );
   }
